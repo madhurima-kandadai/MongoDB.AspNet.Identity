9ee407ef60ec32f9dd2188df0628a6f71c3c5cf2
Madhurima Kandadai <madhurima_kandadai@epam.com>
2017-04-18 10:49:29 +0530

    minor changes !!

--- WebApplication1/Models/IdentityModels.cs
+++ WebApplication1/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using MongoDB.AspNet.Identity;
@@ -12,11 +13,20 @@ namespace WebApplication1.Models
         public object PhoneNumber { get; internal set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
+        {            
+            try
+            {
+                // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+                // Add custom user claims here
+                return userIdentity;
+            }
+            catch(Exception ex)
+            {
+                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+                // Add custom user claims here
+                return userIdentity;
+            }
         }
     }
 
